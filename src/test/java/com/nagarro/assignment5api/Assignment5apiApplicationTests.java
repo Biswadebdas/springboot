2833b16cc0072c2f27de806417000dc73eaebd98
Biswadebdas <127171770+Biswadebdas@users.noreply.github.com>
2023-05-15 11:10:56 +0530

    Update Assignment5apiApplicationTests.java

--- src/test/java/com/nagarro/assignment5api/Assignment5apiApplicationTests.java
+++ src/test/java/com/nagarro/assignment5api/Assignment5apiApplicationTests.java
@@ -3,11 +3,9 @@ package com.nagarro.assignment5api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class Assignment5apiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
